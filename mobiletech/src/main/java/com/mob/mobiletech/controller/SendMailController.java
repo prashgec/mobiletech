bee1584bf6bb61960cf62e3c865dc9ded6c35bc2
Sushil <Sushil@Sushil-VAIO>
2014-03-10 23:57:43 +0530

    from sushil

--- mobiletech/src/main/java/com/mob/mobiletech/controller/SendMailController.java
+++ mobiletech/src/main/java/com/mob/mobiletech/controller/SendMailController.java
@@ -1,9 +1,12 @@
-/*package com.mob.mobiletech.controller;
+package com.mob.mobiletech.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.mail.MailMessage;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
@@ -12,50 +15,39 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.mob.mobiletech.domain.User;
+import com.mob.mobiletech.services.MailMail;
 
 //	import com.mob.service.ApplicationMailer1;
 
-	*//**
+	/**
 	 * @author Sushil
 	 *
-	 *//*
+	 */
 	@Controller
-	@RequestMapping("/SendMail")
+	@RequestMapping(value ="/SendMail")
 	public class SendMailController {
-	//	@Autowired
-			private JavaMailSenderImpl mailSender;
+		@Autowired
+		//private MailSender mailSender;
+		private MailMail mailMail;
+		public void setMailMail(MailMail mailMail) {
+			this.mailMail = mailMail;
+		}
 		@RequestMapping(method = RequestMethod.POST)
 		public ModelAndView sendEmail(HttpServletRequest request) {
-			SimpleMailMessage message = new SimpleMailMessage();
-	        message.setTo("kuldeepkuldeepjain@yahoo.co.in");
-	        message.setSubject("Regarding MobileTech Enquiry");
+		  
+			
 	        String text="Name: " + request.getParameter("name") + "\n" +"Contact: " +request.getParameter("contact")+
 	        		     "\nEmail: " + request.getParameter("email") + "\nMessage: " + request.getParameter("message");
-	        message.setText(text);
-	        System.out.println("Text : " + text);
-	        System.out.println("TO " + message.getTo()[0] + "Subject " + message.getSubject());
-	        if(mailSender == null)
-	        	System.out.println("can't send the mail");
-	        else {
-	            mailSender.send(message);
-	        }
-			//Create the application context
-	      //  ApplicationContext context = new FileSystemXmlApplicationContext("WebContent/WEB-INF/spring-servlet.xml");
-	         
-	        //Get the mailer instance
-	        //ApplicationMailer mailer = new ApplicationMailer();//(ApplicationMailer) context.getBean("mailService");
-	 
-	        //Send a composed mail
-	        //mailer.sendMail("Sushiltilak00@gmail.com", "Test Subject", "Testing body");
 			
+	        mailMail.sendMail("mobiletech@gmail.com",
+	    		   "admin@mobiletech.co.in",
+	    		   "Enquiry On MobileTech", 
+	    		   text);
 			
-			
-			String message1 = "Enquiry Submitted... You may get a call soon.";
-			return new ModelAndView("enquirySuccess", "message", message1);
+	   		return new ModelAndView("enquiry","loginForm",new User());
 		}
 
 
 	}
 	
 
-*/
